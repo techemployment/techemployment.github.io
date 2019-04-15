744d475f588da5e82be307b83bab7e8b3665a64d
test <test@test.com>
2019-04-15 15:37:18 -0400

    added somethings i worked on in class today

---
+++ 4:12/countingCharacters.js
@@ -0,0 +1,74 @@
+/*
+function countingCharacters(stringToCount){
+    // The length property has been mentioned in
+    // the Logging Letter Exercise in Lesson 4
+    console.log (stringToCount + " has " +
+              stringToCount.length + " characters.");
+}
+
+function countingCharacters2(stringToCount, characterToFind){
+  var characterCount=0;
+  for (var characterPosition=0;
+      characterPosition<stringToCount.length;
+      characterPosition++){
+    if(stringToCount[characterPosition]==characterToFind){
+      characterCount++;
+    }
+  }
+  console.log("String to search in:" + stringToCount);
+  console.log("Character to find:" + characterToFind);
+  console.log("Number of times the character appears:" +  characterCount);
+
+}
+
+
+function countingCharacters3(str,search) {
+  var count = 0;
+  var numChars = search.length;
+  var lastIndex = str.length - numChars;
+
+  for(var index = 0;index <= lastIndex; index ++){
+    var current = str.substring(index,index+numChars);
+  if(current==search){
+    count++;
+  }
+}
+return count;
+
+console.log("String to search in:" +str);
+console.log("Character to find:" +search);
+console.log("Number of times the character appears:" +count);
+}
+*/ /*
+Math.random() * 6;
+for(var i=0; i<100; i++) {
+  console.log(Math.floor(Math.random() *6)+1);
+}
+*/
+/*
+function rollDice(numSides) {
+  return Math.floor(Math.random() * numSides) +1;
+}
+*/
+//for (var i=0; i<100; i++){
+  //console.log(rollDice());
+
+}
+/*
+function addTwoNumbers(firstNumber, secondNumber){
+
+    var firstNumber= 5;
+    var secondNumber=10;
+}
+*/
+
+var team1=new Array();
+var team2=new Array();
+
+for(var i=0; i<friends.length; i++){
+  if(i%2==0){
+
+}else{
+  
+}
+}
