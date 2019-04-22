9a0ea29c99cbfa2ce8349a7023e7cf97605398c7
test <test@test.com>
2019-04-22 07:46:12 -0400

    display evens in the folder

---
+++ work on till tuesday/displayEvens.js
@@ -0,0 +1,25 @@
+var startingNumber;
+var endingNumber;
+var step;
+
+function count (){
+  prompt("put in start value " + startingNumber + " put in end value " + endingNumber + " and an increment " + step);
+
+  if(endingNumber > startingNumber){
+    for(startingNumber=0; startingNumber <=endingNumber; step = startingNumber + step)
+
+  }else{
+    alert("put in different values");
+  }
+
+}
+  count();
+console.log(startingNumber);
+  console.log(endingNumber);
+  console.log(step);
+
+  console.log()
+  2%==0
+
+  var range ;
+  if(range )
