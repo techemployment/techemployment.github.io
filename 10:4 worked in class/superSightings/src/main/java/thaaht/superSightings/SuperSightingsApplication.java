2f9653c3bbb25d2eb0dad7e0e3242b80f283b1bf
test <test@test.com>
2019-10-04 15:55:06 -0400

    stuff in class

---
+++ 10:4 worked in class/superSightings/src/main/java/thaaht/superSightings/SuperSightingsApplication.java
@@ -0,0 +1,13 @@
+package thaaht.superSightings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSightingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperSightingsApplication.class, args);
+	}
+
+}
---
+++ 10:4 worked in class/superSightings/src/test/java/thaaht/superSightings/SuperSightingsApplicationTests.java
@@ -0,0 +1,13 @@
+package thaaht.superSightings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSightingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
